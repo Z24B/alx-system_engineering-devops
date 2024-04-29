8addddfe29a0a2641c23bd99951afeeb62de14d8
Z24B <zivha.beharie@gmail.com>
2024-04-29 08:29:31 -0700

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,29 @@
 # Puppet manifest to add a custom HTTP header to Nginx
 
 # Define the package resource to install Nginx
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
+}
+
+# install nginx web server on server
 package { 'nginx':
-  ensure => 'installed',
+  ensure   => present,
+  provider => 'apt'
 }
 
 # Define the file resource to add custom HTTP header to Nginx configuration
-file { '/etc/nginx/sites-available/custom_header':
-  ensure  => 'file',
-  content => "server {
-    listen 80;
-    server_name _;
-
-    location / {
-      add_header X-Served-By $::hostname;
-    }
-  }",
-  require => Package['nginx'], # Ensure Nginx package is installed before configuring
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
 }
 
-# Define the symbolic link resource to enable the custom configuration
-file { '/etc/nginx/sites-enabled/custom_header':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/custom_header',
-  require => File['/etc/nginx/sites-available/custom_header'],
-  notify => Service['nginx'], # Notify Nginx service to reload configuration
-}
-
-# Define the service resource for Nginx
+# start service
 service { 'nginx':
-  ensure => 'running',
-  enable => true,
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
 }
