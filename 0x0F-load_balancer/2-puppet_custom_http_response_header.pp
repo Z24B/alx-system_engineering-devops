06e250792d467bc3a00f136233186e7216549c90
Z24B <zivha.beharie@gmail.com>
2024-04-29 08:20:53 -0700

    Task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,29 @@
+# Puppet manifest to add a custom HTTP header to Nginx
+
+# Define the package resource to install Nginx
+package { 'nginx':
+  ensure => 'installed',
+}
+
+# Define the file resource to add custom HTTP header to Nginx configuration
+file { '/etc/nginx/sites-available/custom_header':
+  ensure  => 'file',
+  content => "server {
+    listen 80;
+    server_name _;
+
+    location / {
+      add_header X-Served-By $::hostname;
+    }
+  }",
+  require => Package['nginx'], # Ensure Nginx package is installed before configuring
+}
+
+# Define the symbolic link resource to enable the custom configuration
+file { '/etc/nginx/sites-enabled/custom_header':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/custom_header',
+  notify => Service['nginx'],
+  require => File['/etc/nginx/sites-available/custom_header'],
+}
+
