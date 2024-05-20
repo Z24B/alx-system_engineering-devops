45d688ca1ffc157f278b0314e294fe9056f8856f
Z24B <zivha.beharie@gmail.com>
2024-05-20 06:19:41 -0700

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,54 +1,40 @@
 #!/usr/bin/python3
-"""
-Gathers data from an API for a given employee ID and displays their TODO list progress.
-"""
+"""Script to gather data from an API"""
 
 import requests
 import sys
 
-users_url = "https://jsonplaceholder.typicode.com/users"
-todos_url = "https://jsonplaceholder.typicode.com/todos"
-
-
-def gather_data(employee_id):
-    """Fetch employee TODO list progress"""
-    # Fetch user info
-    user_response = requests.get(users_url + f'/{employee_id}')
-    if user_response.status_code != 200:
-        print("User not found")
-        return
-
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
-
-    # Fetch user's TODO list
-    todo_response = requests.get(todos_url, params={'userId': employee_id})
-    if todo_response.status_code != 200:
-        print("TODO list not found")
-        return
-
-    todo_data = todo_response.json()
-
-    # Count completed tasks
-    completed_tasks = [task for task in todo_data if task['completed']]
-    total_tasks = len(todo_data)
-    completed_count = len(completed_tasks)
-
-    # Display progress
-    print(f"Employee {employee_name} is done with tasks({completed_count}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
-
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer")
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    if user_response.status_code != 200:
+        print("Error: User data not found")
         sys.exit(1)
 
-    gather_data(employee_id)
+    if todos_response.status_code != 200:
+        print("Error: TODO list not found")
+        sys.exit(1)
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+
+    employee_name = user_data.get('name')
+    total_tasks = len(todos_data)
+    completed_tasks = [task for task in todos_data if task.get('completed')]
+    num_completed_tasks = len(completed_tasks)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, num_completed_tasks, total_tasks))
+    for task in completed_tasks:
+        print("\t{}".format(task.get('title')))
