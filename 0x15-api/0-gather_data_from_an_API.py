ed4e41f709037711c418a715734ffeab55b1a462
Z24B <zivha.beharie@gmail.com>
2024-05-20 06:07:52 -0700

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,41 +1,54 @@
 #!/usr/bin/python3
 """
-Checks student output for returning info from REST API
+Gathers data from an API for a given employee ID and displays their TODO list progress.
 """
 
 import requests
 import sys
 
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-def check_tasks(id):
-    """ Fetch user name, number of tasks """
 
-    user_url = f"https://jsonplaceholder.typicode.com/users/{id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
+def gather_data(employee_id):
+    """Fetch employee TODO list progress"""
+    # Fetch user info
+    user_response = requests.get(users_url + f'/{employee_id}')
+    if user_response.status_code != 200:
+        print("User not found")
+        return
 
-    try:
-        user_response = requests.get(user_url)
-        user_response.raise_for_status()
-        user_data = user_response.json()
+    user_data = user_response.json()
+    employee_name = user_data.get('name')
 
-        todo_response = requests.get(todo_url)
-        todo_response.raise_for_status()
-        todo_data = todo_response.json()
+    # Fetch user's TODO list
+    todo_response = requests.get(todos_url, params={'userId': employee_id})
+    if todo_response.status_code != 200:
+        print("TODO list not found")
+        return
 
-        done_tasks = [task for task in todo_data if task['completed']]
-        total_tasks = len(todo_data)
-        num_done_tasks = len(done_tasks)
+    todo_data = todo_response.json()
 
-        print("Employee {} is done with tasks({}/{}):".format(
-            user_data['name'], num_done_tasks, total_tasks))
+    # Count completed tasks
+    completed_tasks = [task for task in todo_data if task['completed']]
+    total_tasks = len(todo_data)
+    completed_count = len(completed_tasks)
 
-        for task in done_tasks:
-            print("\t{}".format(task['title']))
-
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
-        sys.exit(1)
+    # Display progress
+    print(f"Employee {employee_name} is done with tasks({completed_count}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
 
 
 if __name__ == "__main__":
-    check_tasks(int(sys.argv[1]))
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
+
+    gather_data(employee_id)
